281d3ec9ed908d6f64d05f1b249f1789a3f84845
Mostafa Abdel-Mohsen Mostafa Abdel-Mohsen Essa <150021021+mostafamohsen1@users.noreply.github.com>
2026-08-17 01:24:44 +0300

    Add files via upload

---
+++ crowdfunding/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.core.urls')),
+    path('accounts/', include('apps.accounts.urls')),
+    path('projects/', include('apps.projects.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Custom Error Handlers
+handler404 = 'apps.core.views.custom_404_view'
+handler500 = 'apps.core.views.custom_500_view'
